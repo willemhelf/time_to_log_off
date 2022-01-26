576762639a8c9793bd08973fe5a990c667b4162a
sophiehelf <sophie.helf@gmail.com>
2022-01-26 16:02:23 -0500

    renamed variables, added quick test

--- tweet_machine/tweet_poster.py
+++ tweet_machine/tweet_poster.py
@@ -1,12 +1,19 @@
 import tweepy
 import config
 
-auth = tweepy.OAuthHandler(config.client_id, config.client_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
 
 # Create API object
 api = tweepy.API(auth)
 
 # Send a tweet
+
+#test 
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print(":(")
 
 api.update_status("made a janky little thing to post tweets via code editor/terminal... coding/API knowledge very much required lol... have fun https://github.com/sophiehelf/time_to_log_off")
